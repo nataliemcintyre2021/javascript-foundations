1674d4f9d1589fd5c9b1580138d11a57259c5f15
Natalie McIntyre <nataliemcintyre2021@gmail.com>
2021-05-21 11:20:18 -0700

    Add dragon exercises

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon {
+  constructor(name, rider) {
+  this.name = name;
+  this.rider = rider;
+  this.hungry = true;
+  this.counter = 0
+  }
 
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+
+  eat() {
+    if (this.counter === 0 || this.counter === 1) {
+      this.counter++
+  } else if (this.counter === 2) {
+      return this.hungry = false;
+  }
+}
+}
+
+
+module.exports = Dragon;
